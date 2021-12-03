34167799af78d12391f55919450b6ec76a6fa0a3
ctal <c_tallent@sbcglobal.net>
2021-12-03 15:31:24 -0600

    Updated code

---
+++ Temperature.py
@@ -0,0 +1,7 @@
+temperature = int(input("What is the temperature outside? "))
+
+if temperature > 80: #create user input to test if/then conditional statement
+    print("Turn on the AC.")
+else:
+    print("Open the windows.")  
+
